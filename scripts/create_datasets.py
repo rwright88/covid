a3090ed06fbbca032462b9bd8488f5e79b70de35
Ray Wright <raywright88@gmail.com>
2020-09-27 09:30:52 -0400

    Update script

--- scripts/create_datasets.py
+++ scripts/create_datasets.py
@@ -18,27 +18,27 @@ HEROKU_APP = "rwright-covid"
 
 
 def main():
-    start = time.time()
+    t0 = time.time()
     dir_name = os.path.dirname(OUT_DATA)
     if not os.path.isdir(dir_name):
         os.makedirs(dir_name, exist_ok=True)
     df = covid.get_data(n=N)
     df = df[df["pop"].notna()]
     df = df.round(2)
-    print(f"Getting data took: {str(int(time.time() - start))} seconds")
+    print(f"Getting data took: {str(int(time.time() - t0))} seconds")
 
-    start = time.time()
+    t0 = time.time()
     df.to_csv(OUT_DATA, index=False)
-    print(f"Writing file took: {str(int(time.time() - start))} seconds")
+    print(f"Writing file took: {str(int(time.time() - t0))} seconds")
 
-    start = time.time()
+    t0 = time.time()
     s3 = boto3.resource("s3")
     s3.meta.client.upload_file(OUT_DATA, S3_BUCKET, S3_OBJECT)
-    print(f"Uploading file to S3 took: {str(int(time.time() - start))} seconds")
+    print(f"Uploading file to S3 took: {str(int(time.time() - t0))} seconds")
 
-    start = time.time()
-    subprocess.run(["heroku", "dyno:restart", "--app", HEROKU_APP])
-    print(f"Restarting Heroku dyno took: {str(int(time.time() - start))} seconds")
+    t0 = time.time()
+    subprocess.run(["heroku", "dyno:restart", "--app", HEROKU_APP], shell=True)
+    print(f"Restarting Heroku dyno took: {str(int(time.time() - t0))} seconds")
 
 
 if __name__ == "__main__":
